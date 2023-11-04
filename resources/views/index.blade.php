c9a5bf0a3619444138f8d1f0187c452d542222c3
niloofar-samei <niloofar.samei@gmail.com>
2023-11-04 22:25:19 +0000

    Add search form to the home page.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,15 @@
 @extends('master')
 
 @section('content')
-	<h1>hi there</h1>
+	
+	<div style="background-color: white; opacity: 0.5; border: 1px solid yellow; width: 800px; height: 500px; margin: auto; margin-top: 100px;">
+		
+		{!! Form:open(['route' => '', 'method' => 'post']) !!}
+
+			{{ Form::text('search', ['placeholder'=>'Search for room availability']) }}
+
+		{!! Form::close() !!}
+
+	</div>
+
 @endsection
