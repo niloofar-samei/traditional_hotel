e76b7a9761df42bc7aa83a1cd431fd970aa92faa
niloofar-samei <niloofar.samei@gmail.com>
2023-11-10 22:32:20 +0000

    Edit navbar in the reservation file.

--- resources/views/reservation.blade.php
+++ resources/views/reservation.blade.php
@@ -21,21 +21,22 @@
 <body>
 
 	{{-- navbar --}}
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-	  <a class="navbar-brand" href="#">Navbar</a>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="padding-left: 30px">
+
+	  <a class="navbar-brand" href="{{url('/hotel')}}">TRADITONAL HOTEL</a>
+
 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 	    <span class="navbar-toggler-icon"></span>
 	  </button>
-	  <div class="collapse navbar-collapse" id="navbarNav">
+
+	  <div class="collapse navbar-collapse" id="navbarNav" style="padding-left: 50px;">
 	    <ul class="navbar-nav">
-	      <li class="nav-item active">
-	        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-	      </li>
 	      <li class="nav-item">
-	        <a class="nav-link" href="#">Features</a>
+	        <a class="nav-link" href="{{url('/reservation')}}">RESERVATION</a>
 	      </li>
 	    </ul>
 	  </div>
+
 	</nav>
 
 
