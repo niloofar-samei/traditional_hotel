9ee2453b7378e598fa07d44b013ccfd023cbb8fa
niloofar-samei <niloofar.samei@gmail.com>
2023-12-23 00:40:28 +0000

    Edit reservationRequest and guestController imports.

--- app/Http/Requests/reservationRequest.php
+++ app/Http/Requests/reservationRequest.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Request;
 
-class reservationRequest extends Request
+class reservationRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
