97b89460914bd03d8f9c19788f307b52da96d2c9
niloofar-samei <niloofar.samei@gmail.com>
2024-01-04 14:18:25 +0000

    Create rooms migration file.

--- database/migrations/2024_01_04_123752_create_rooms_table.php
+++ database/migrations/2024_01_04_123752_create_rooms_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('rooms', function (Blueprint $table) {
             $table->id();
+            $table->string('room_number');
+            $table->string('room_type');
+            $table->boolean('status')->default(0);
             $table->timestamps();
         });
     }
