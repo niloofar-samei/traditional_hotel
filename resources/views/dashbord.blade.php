889853c53c5854a4eb5a7c990cccf6479f7eb35b
niloofar-samei <niloofar.samei@gmail.com>
2024-01-05 21:08:27 +0000

    Add table to dashbord page.

--- resources/views/dashbord.blade.php
+++ resources/views/dashbord.blade.php
@@ -46,7 +46,32 @@
 
 	<div class="container text-center" style="margin-top: 50px">
 
-
+		<table class="table table-hover table-dark">
+			<tr>
+				<td>id</td>
+				<td>name</td>
+				<td>family</td>
+				<td>nationality</td>
+				<td>idcode</td>
+				<td>mobile</td>
+				<td>room number</td>
+				<td>room type</td>
+				<td>created at</td>
+				<td>updated at</td>
+			</tr>
+			<tr>
+				<td>{{ $new_guest->id }}</td>
+				<td>{{ $new_guest->first_name }}</td>
+				<td>{{ $new_guest->last_name }}</td>
+				<td>{{ $new_guest->nationality }}</td>
+				<td>{{ $new_guest->id_code }}</td>
+				<td>{{ $new_guest->mobile }}</td>
+				<td>{{ $new_guest->room_number}}</td>
+				<td>{{ $new_guest->room_type }}</td>
+				<td>{{ $new_guest->created_at }}</td>
+				<td>{{ $new_guest->updated_at }}</td>
+			</tr>
+		</table>
 
 	</div> {{-- end of div container --}}
 
