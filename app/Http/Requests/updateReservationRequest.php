b44abdc93894fe1c3de1175e1f529cfd3ef210a1
niloofar-samei <niloofar.samei@gmail.com>
2024-01-06 15:02:02 +0000

    Edit updateReservationRequest to remove unique.

--- app/Http/Requests/updateReservationRequest.php
+++ app/Http/Requests/updateReservationRequest.php
@@ -26,8 +26,8 @@ class updateReservationRequest extends FormRequest
             "first_name"    => "required",
             "last_name"     => "required",
             "nationality"   => "required",
-            "id_code"       => "required|unique:guests",
-            "mobile"        => "required|unique:guests"
+            "id_code"       => "required",
+            "mobile"        => "required"
         ];
     }
 }
