e784cfc57f7135dba3cb511960c473be6d6adbe1
niloofar-samei <niloofar.samei@gmail.com>
2024-01-06 00:51:19 +0000

    Add extends and section to reservation file.

--- resources/views/reservation.blade.php
+++ resources/views/reservation.blade.php
@@ -1,56 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="{{asset('dist/css/bootstrap.min.css')}}">
-	<title></title>
-	<script src="{{asset('dist/js/jquery-3.7.1.min.js')}}"></script>
-	<script src="{{asset('dist/js/popper.min.js')}}"></script>
-	<script src="{{asset('dist/js/bootstrap.min.js')}}"></script>
+@extends('master')
 
-	<style>
-		.dropbtn {
-				background-color: #4CAF50;
-				color: white;
-				padding: 16px;
-				font-size: 16px;
-				border: none;
-				cursor: pointer;
-			}
-	</style>
-<script>
-
-{{--
-$(document).ready(function() {
-	$('#q').click(function() {
-		$("#more-form").toggle(100);
-	});
-});
---}}
-</script>
-
-</head>
-<body>
-
-	{{-- navbar --}}
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="padding-left: 30px">
-
-		<a class="navbar-brand" href="{{url('/hotel')}}">TRADITONAL HOTEL</a>
-
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-
-		<div class="collapse navbar-collapse" id="navbarNav" style="padding-left: 50px;">
-			<ul class="navbar-nav">
-				<li class="nav-item">
-					<a class="nav-link" href="{{url('/reservation')}}">RESERVATION</a>
-				</li>
-			</ul>
-		</div>
-
-	</nav>
+@section('content')
 
 	<a href="{{url('/dashbord')}}">your dashbord</a>
 
@@ -134,5 +84,4 @@ $(document).ready(function() {
 		</div>
 	</div>
 
-</body>
-</html>
+@endsection
