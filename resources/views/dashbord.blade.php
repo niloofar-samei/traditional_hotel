833be7dae900c697e483d8fbb8be506391eed20d
niloofar-samei <niloofar.samei@gmail.com>
2024-01-06 01:01:05 +0000

    Clean up dashbord file.

--- resources/views/dashbord.blade.php
+++ resources/views/dashbord.blade.php
@@ -1,48 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="{{asset('dist/css/bootstrap.min.css')}}">
-	<title></title>
-	<script src="{{asset('dist/js/jquery-3.7.1.min.js')}}"></script>
-	<script src="{{asset('dist/js/popper.min.js')}}"></script>
-	<script src="{{asset('dist/js/bootstrap.min.js')}}"></script>
+@extends('dashbord.master')
 
-	<style>
-		.dropbtn {
-				background-color: #4CAF50;
-				color: white;
-				padding: 16px;
-				font-size: 16px;
-				border: none;
-				cursor: pointer;
-			}
-	</style>
-
-</head>
-<body>
-
-	{{-- navbar --}}
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="padding-left: 30px">
-
-		<a class="navbar-brand" href="{{url('/hotel')}}">TRADITONAL HOTEL</a>
-
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-
-		<div class="collapse navbar-collapse" id="navbarNav" style="padding-left: 50px;">
-			<ul class="navbar-nav">
-				<li class="nav-item">
-					<a class="nav-link" href="{{url('/reservation')}}">RESERVATION</a>
-				</li>
-			</ul>
-		</div>
-
-	</nav>
-
-	<div class="container text-center" style="margin-top: 50px">
+@section('content')
 
 		@if(Session::has('update'))
 
@@ -83,7 +41,4 @@
 			</tr>
 		</table>
 
-	</div> {{-- end of div container --}}
-
-</body>
-</html>
+@endsection
---
+++ resources/views/dashbord/master.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="{{asset('dist/css/bootstrap.min.css')}}">
+	<title></title>
+	
+	<script src="{{asset('dist/js/jquery-3.7.1.min.js')}}"></script>
+	<script src="{{asset('dist/js/popper.min.js')}}"></script>
+	<script src="{{asset('dist/js/bootstrap.min.js')}}"></script>
+</head>
+<body>
+
+	{{-- navbar --}}
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="padding-left: 30px">
+
+		<a class="navbar-brand" href="{{url('/hotel')}}">TRADITONAL HOTEL</a>
+
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarNav" style="padding-left: 50px;">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link" href="{{url('/reservation')}}">RESERVATION</a>
+				</li>
+			</ul>
+		</div>
+
+	</nav>
+
+	{{-- container --}}
+	<div class="container text-center" style="margin-top: 50px">
+
+		@yield('content')
+	
+	</div>
+
+</body>
+</html>
