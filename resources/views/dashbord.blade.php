bcaff94d3b042187407e6b176f2ecd95cb46b8aa
niloofar-samei <niloofar.samei@gmail.com>
2024-01-07 20:59:57 +0000

    Edit dashbord css and text.

--- resources/views/dashbord.blade.php
+++ resources/views/dashbord.blade.php
@@ -7,8 +7,8 @@
 	{{-- guest information table --}}
 	<table class="table table-hover table-dark">
 		<tr>
-			<td>delete</td>
-			<td>update</td>
+			<td>Cancelation</td>
+			<td>Inofarmation</td>
 			<td>id</td>
 			<td>name</td>
 			<td>family</td>
@@ -24,11 +24,11 @@
 			<td>
 				{!! Form::open(['route'=>['guest.destroy', $new_guest->id], 'method'=>'delete']) !!}
 
-					{!! Form::submit('delete') !!}
+					{!! Form::submit('Cancel my room', ['class' => 'btn btn-danger']) !!}
 
 				{!! Form::close() !!}
 			</td>
-			<td><a href="{{url('guest/'.$new_guest->id.'/edit')}}">update</a></td>
+			<td><a href="{{url('guest/'.$new_guest->id.'/edit')}}" class="btn btn-warning">update</a></td>
 			<td>{{ $new_guest->id }}</td>
 			<td>{{ $new_guest->first_name }}</td>
 			<td>{{ $new_guest->last_name }}</td>
@@ -36,10 +36,16 @@
 			<td>{{ $new_guest->id_code }}</td>
 			<td>{{ $new_guest->mobile }}</td>
 			<td>{{ $new_guest->room_number}}</td>
-			<td>{{ $new_guest->room_type }}</td>
+			<td>
+				<?php
+					$a = explode('_', $new_guest->room_type);
+					echo "$a[0] $a[1]";
+				?>
+			</td>
 			<td>{{ $new_guest->created_at }}</td>
 			<td>{{ $new_guest->updated_at }}</td>
 		</tr>
 	</table>
 
 @endsection
+
