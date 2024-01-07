714ae254fa7215d74e35b71a846ab9d31f89bc80
niloofar-samei <niloofar.samei@gmail.com>
2024-01-07 20:38:28 +0000

    Edit reservation form.

--- resources/views/reservation.blade.php
+++ resources/views/reservation.blade.php
@@ -18,10 +18,10 @@
 	{{-- reservation form --}}
 	<div style="margin: auto; width: 600px; background-image: linear-gradient(to right, #f46b45, #eea849); padding-top: 50px; padding-bottom: 20px; border-radius: 20px; box-shadow: 0px 0px 30px 5px black;">
 
-		<h3 style="border: 1px solid black">Please reserve your room!</h3>
+		<h3>Please reserve your room!</h3>
 		{{-- <p id="q" style="border: 1px solid black">More Guests?</p> --}}
 
-		<div style="border: 1px solid green" id="single-form">
+		<div id="single-form">
 			{!! Form::open(['route'=>'guest.store', 'method'=>'POST']) !!}
 
 			<div class="form-group" style="padding: 10px;">
@@ -48,7 +48,7 @@
 				{{ Form::text('room_number', $selected_room, ['placeholder'=>'Room Number', 'style'=>'width: 315px; height: 47px; padding-left: 10px;']) }}
 			</div>
 			
-			<div class="form-group" style="padding: 10px;">
+			<div class="form-group" style="padding: 10px; margin-left: -14px;">
 				<label style="padding-left: 15px;">{!! Form::radio('room_type', 'Single_bed', true, ['id'=>'1']) !!} Single bed</label>
 				<label style="padding-left: 15px;">{!! Form::radio('room_type', 'Twin_bed', true, ['id'=>'2']) !!} Twin bed</label>
 				<label style="padding-left: 15px;">{!! Form::radio('room_type', 'Three_beds', true, ['id'=>'3']) !!} Three beds</label>
