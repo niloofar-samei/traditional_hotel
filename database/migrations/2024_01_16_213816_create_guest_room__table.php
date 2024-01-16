100f022b1c8a96fee229af13be392501a93ea063
niloofar-samei <niloofar.samei@gmail.com>
2024-01-16 22:42:21 +0000

    Create guest_room migration file.

---
+++ database/migrations/2024_01_16_213816_create_guest_room__table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('guest_room', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('room_id')
+            $table->foreign('room_id')->references('id')->on('rooms')
+            $table->unsignedBigInteger('guest_id')
+            $table->foreign('guest_id')->references('id')->on('guests')
+            $table->timestamp();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('guest_room', function (Blueprint $table) {
+            //
+        });
+    }
+};
