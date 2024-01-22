e2c9dbd699f1a43547ae2822491dcbb4d7c12634
niloofar-samei <niloofar.samei@gmail.com>
2024-01-22 20:04:13 +0000

    Change some parts.

--- database/seeders/RoomTableSeeder.php
+++ database/seeders/RoomTableSeeder.php
@@ -12,12 +12,12 @@ class RoomTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //\Illuminate\Support\Facades\DB::table('rooms')->truncate();
         for($i=0;$i<15;$i++) {
 
             \App\Models\Room::create([
-                "room_type" => "bed",
-                //if($i == 4){"single_bed"}elseif($i == 9){"twin_bed"}else{"third_bed"},
+                "room_type" =>
+                    if($i == 4){"single_bed"}elseif($i == 9){"twin_bed"}else{"third_bed"},
+                }
                 "status"    =>  0]);
         }   
     }
