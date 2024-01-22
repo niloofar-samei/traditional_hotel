abf9cc2c98cda9d13394b2eb98153caafb1e8016
niloofar-samei <niloofar.samei@gmail.com>
2024-01-22 20:51:33 +0000

    Edit seeder for Room table.

--- database/seeders/RoomTableSeeder.php
+++ database/seeders/RoomTableSeeder.php
@@ -12,13 +12,26 @@ class RoomTableSeeder extends Seeder
      */
     public function run(): void
     {
-        for($i=0;$i<15;$i++) {
 
-            \App\Models\Room::create([
-                "room_type" =>
-                    if($i == 4){"single_bed"}elseif($i == 9){"twin_bed"}else{"third_bed"},
-                }
-                "status"    =>  0]);
+        //\Illuminate\Support\Facades\DB::table('rooms')->truncate();
+        for($i=0;$i<30;$i++) {
+
+            if($i < 10) {
+                \App\Models\Room::create([
+                    "room_type" => "single_bed",
+                    //if($i == 4){"single_bed"}elseif($i == 9){"twin_bed"}else{"third_bed"},
+                    "status"    =>  0]);
+            }elseif($i < 20) {
+                \App\Models\Room::create([
+                    "room_type" => "twin_bed",
+                    //if($i == 4){"single_bed"}elseif($i == 9){"twin_bed"}else{"third_bed"},
+                    "status"    =>  0]);
+            }else{
+                \App\Models\Room::create([
+                    "room_type" => "third_bed",
+                    //if($i == 4){"single_bed"}elseif($i == 9){"twin_bed"}else{"third_bed"},
+                    "status"    =>  0]);
+            }
         }   
     }
 }
