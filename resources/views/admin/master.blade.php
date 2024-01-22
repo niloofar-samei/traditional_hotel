20d6b477656607234e5531f8723f0f01be4c191a
niloofar-samei <niloofar.samei@gmail.com>
2024-01-22 17:14:23 +0000

    Create admin.master blade file.

---
+++ resources/views/admin/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+
+	{{-- container --}}
+	<div class="container text-center" style="margin-top: 50px">
+	
+		@yield('content')
+
+	</div>
+
+</body>
+</html>
