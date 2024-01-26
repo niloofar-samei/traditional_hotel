a19494a9c5b198ee3ffca028878825208590b118
niloofar-samei <niloofar.samei@gmail.com>
2024-01-26 21:02:49 +0000

    Add login and register to navbar. Fix nav collaps icon problem.

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -14,15 +14,15 @@
 	<!--
 	<script>
 		jQuery(document).ready(function() {
-    		var foo = jQuery('#foo');
+			var foo = jQuery('#foo');
   
-    		function updateTime() {
-        		var now =  new Date();
-        		foo.val(now.toUTCString());    
-    		}
+			function updateTime() {
+				var now =  new Date();
+				foo.val(now.toUTCString());    
+			}
   
-    		updateTime();
-    		setInterval(updateTime, 1000);
+			updateTime();
+			setInterval(updateTime, 1000);
 		});
 	</script>
 	-->
@@ -33,24 +33,64 @@
 	{{-- navbar --}}
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="padding-left: 30px">
 
-	  <a class="navbar-brand" href="{{url('/hotel')}}">TRADITONAL HOTEL</a>
+		<a class="navbar-brand" href="{{url('/hotel')}}">TRADITONAL HOTEL</a>
 
-	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-	    <span class="navbar-toggler-icon"></span>
-	  </button>
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+			<span class="navbar-toggler-icon"></span>
+		</button>
 
-	  <div class="collapse navbar-collapse" id="navbarNav" style="padding-left: 50px; border: 1px solid red;">
-	    <ul class="navbar-nav">
-	      <li class="nav-item">
-	        <a class="nav-link" href="{{url('/reservation')}}">RESERVATION</a>
-	      </li>
-	    </ul>
-	  </div>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-	  	<!--
-	  	<div>
-      		<input id="foo" name="foo" type="text" value="" size="25" style="border: none; background-color: transparent; color: black">
-      	</div>
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link" href="{{url('/reservation')}}">RESERVATION</a>
+				</li>
+			</ul>
+
+			<!-- Right Side Of Navbar -->
+			<ul class="navbar-nav ms-auto">
+
+				<!-- Authentication Links -->
+				@guest
+					@if (Route::has('login'))
+						<li class="nav-item">
+							<a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+						</li>
+					@endif
+
+					@if (Route::has('register'))
+						<li class="nav-item">
+							<a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+						</li>
+					@endif
+				@else
+					<li class="nav-item dropdown">
+						<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+							{{ Auth::user()->name }}
+						</a>
+
+						<div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="{{ route('logout') }}"
+							   onclick="event.preventDefault();
+							   document.getElementById('logout-form').submit();">
+								{{ __('Logout') }}
+							</a>
+
+							<form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+								@csrf
+							</form>
+						</div>
+					</li>
+				@endguest
+
+			</ul>
+
+		</div>
+
+		<!--
+		<div>
+			<input id="foo" name="foo" type="text" value="" size="25" style="border: none; background-color: transparent; color: black">
+		</div>
 		-->
 
 	</nav>
